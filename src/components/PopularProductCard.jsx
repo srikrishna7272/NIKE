70778e89a6709f1b69591e93ae9bf71cb444083e
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-04 14:59:00 -0700

    Build popular products section and made changes to navbar.

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,17 @@
+import { star } from "../assets/icons";
+
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="text-montserrat text-xl leading-normal text-slate-gray">(4.5)</p>
+      </div>
+      <h3 className="mt-2 font-palanquin text-2xl font-semibold leading-normal ">{name}</h3>
+      <p className="text-coral-red font-montserrat font-semibold text-2xl mt-2 leading-normal">{price}</p>
+    </div>
+  );
+};
+
+export default PopularProductCard;
